a4467b52b01365af5a3f9a08114efe31343b6a32
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-03-03 15:25:46 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) { 
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (seconds) {
+    console.log(seconds)
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log('Wrong time')
+            break;
+
+        default:
+            console.log(error)
+            break;
+    }
+});
